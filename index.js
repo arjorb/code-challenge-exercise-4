c23a173d6f30b09249385966b95b36528bd67554
John U <arjorb@gmail.com>
2023-06-08 16:47:41 +0200

    change the name of the parameter to be descriptive

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const isPrimeNumber = (num) => {
-    if (num <= 1)  return false;
+const isPrimeNumber = (number) => {
+    if (number <= 1)  return false;
 
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-      if (num % i === 0) return false;
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+      if (number % i === 0) return false;
     }
     return true;
   };
